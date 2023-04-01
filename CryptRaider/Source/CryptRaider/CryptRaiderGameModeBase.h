26fa8cfacf2a17ca9122609c5bd8eca24c66bb9f
Sparking2 <sparking02@yahoo.com.mx>
2023-03-31 23:45:03 -0600

    CryptRaider

---
+++ CryptRaider/Source/CryptRaider/CryptRaiderGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CryptRaiderGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRYPTRAIDER_API ACryptRaiderGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
